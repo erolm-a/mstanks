4a0404778f26faa0363026c7fb35923e88a3cdad
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2019-10-20 11:53:09 +0100

    added deg2rad

--- tools.py
+++ tools.py
@@ -3,6 +3,9 @@ import math
 def rad2deg(x):
     return x*180 / math.pi
 
+def deg2rad(x):
+    return x * math.pi / 180
+
 def rotate_head(cur_x, cur_y, dest_x, dest_y):
     heading = math.atan2(dest_y - cur_y, dest_x - cur_x)
     heading = (rad2deg(heading) - 360) % 360
